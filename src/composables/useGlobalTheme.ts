9df3e14a6cc3c35cdce144a3e2d0cb3d0db68d07
matveybobrov <matveybobrov01@gmail.com>
2024-02-17 12:52:36 +0300

    feat: remove redundant comment

--- src/composables/useGlobalTheme.ts
+++ src/composables/useGlobalTheme.ts
@@ -17,10 +17,6 @@ function toggleTheme() {
   localStorage.setItem('theme', theme.value)
 }
 
-// Синглтон (аналог стора), так как сущности создаются вне хука
-// и существуют сами по себе в единственном экземпляре.
-// Мне такой подход показался удобнее, чем provide/inject,
-// так как приходится писать меньше бойлерплейта
 export default function useGlobalTheme() {
   return { Theme, theme, toggleTheme }
 }
